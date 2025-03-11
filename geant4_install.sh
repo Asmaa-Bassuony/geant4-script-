3b0ca21b284f7ddbd66e13313f7c6237caa8435d
Asmaa-Bassuony <am5645@fayoum.edu.eg>
2025-03-11 02:29:24 +0200

    geant4_install.sh

--- geant4_install.sh
+++ geant4_install.sh
@@ -10,13 +10,9 @@ mkdir geant4.10.04.p02_build geant4.10.04.p02_install
 cd geant4.10.04.p02_install
 echo ' export GEANT4_HOME='$PWD  >> ~/.bashrc
 echo ' source $GEANT4_HOME/bin/geant4.sh' >> ~/.bashrc 
-#echo 'hi'
-#echo $GEANT4_HOME
 cd ../geant4.10.04.p02_build 
 cmake -DCMAKE_INSTALL_PREFIX=../geant4.10.04.p02_install -DGEANT4_INSTALL_DATA=ON -DGEANT4_USE_GDML=ON -DGEANT4_USE_QT=ON -DGEANT4_BUILD_MULTITHREADED=ON  -DGEANT4_USE_OPENGL_X11=ON -DGEANT4_USE_RAYTRACER_X11=ON -DGEASN4_USE_SYSTEM_EXPAT=ON  ../geant4.10.04.p02_src
 make -j$(nproc) #for number of cores 
 make install
-
-#dir is home/asmaa/geant4/geant4.10.07-install then go share for sourceing 
 
 
